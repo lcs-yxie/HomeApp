fd32d1d7e19424a13bbcee1d811fa911fb135a67
Yukun Xie <yxie@lcs.on.ca>
2024-01-09 20:48:00 -0500

    Initial Commit

---
+++ HomeApp/HomeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeAppApp.swift
+//  HomeApp
+//
+//  Created by Yukun Xie on 2024/1/9.
+//
+
+import SwiftUI
+
+@main
+struct HomeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
