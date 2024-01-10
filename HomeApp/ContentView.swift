d7b1a9ff4761a59d9ab876c507abfa18c10815c1
Yukun Xie <yxie@lcs.on.ca>
2024-01-09 21:04:41 -0500

    The title and top tool bar

--- HomeApp/ContentView.swift
+++ HomeApp/ContentView.swift
@@ -9,8 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
+        NavigationStack {
+            
+            //The title
+            VStack{
+                Text("My Home")
+                    .font(.system(size: 40))
+                    .bold()
+                    .padding(.trailing, 200)
+            Spacer()
+                
+                
+                
+            }
+                
+            
+                
+                        .toolbar {
+                            ToolbarItem(placement: .navigationBarTrailing) {
+                                HStack {
+                                    Image(systemName: "waveform.path.ecg")
+                                    Image(systemName: "plus")
+                                    Image(systemName: "ellipsis.circle")
+                                }
+                                .foregroundColor(.black)
+                            }
+                        }
+                        .navigationBarTitleDisplayMode(.inline)
+                }
+                .accentColor(.blue)
+        
+        
+        
+                        
         VStack {
-            Image(systemName: "globe")
+            
+            
+            
+            Image(systemName: "")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
